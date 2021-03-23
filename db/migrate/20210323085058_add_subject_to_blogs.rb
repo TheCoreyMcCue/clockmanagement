aa25d8f56be0fd5043c639fa190e79b7f5e0fbd0
monica andreoli <monicandreoli@gmail.com>
2021-03-23 10:48:50 +0100

    index

---
+++ db/migrate/20210323085058_add_subject_to_blogs.rb
@@ -0,0 +1,5 @@
+class AddSubjectToBlogs < ActiveRecord::Migration[6.1]
+  def change
+    add_column :blogs, :subject, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_142630) do
+ActiveRecord::Schema.define(version: 2021_03_23_085058) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_03_22_142630) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "subject"
   end
 
   create_table "users", force: :cascade do |t|
