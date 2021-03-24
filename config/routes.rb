1c6a04ea81c13699571c5125f7baeb4c19723100
Corey McCue <coreysmccue@gmail.com>
2021-03-24 09:01:33 +0100

    fix pages route

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # devise_for :users
   root to: 'pages#home'
-  resources :pages do
-  end
+  get "/pages/:plug" => "pages#show"
   resources :blogs do
   end
 end
