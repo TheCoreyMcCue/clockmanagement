fd4d25cba5dfe9d93959ff2e80d0fbbbb8b265d1
Corey McCue <coreysmccue@gmail.com>
2021-03-26 09:17:12 +0100

    remove authentication for rankings page

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,4 +1,5 @@
 class RankingsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
   def index
     @blogs = policy_scope(Blog).order(created_at: :desc)
     @ranking = Ranking.first
