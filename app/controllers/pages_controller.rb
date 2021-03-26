02267a3919a24d871761976b868298a34baf6d37
Corey McCue <coreysmccue@gmail.com>
2021-03-26 10:40:34 +0100

    unbreak ranking page and unlock about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :plug, :podcast]
+  skip_before_action :authenticate_user!, only: [:home, :plug, :podcast, :about]
 
   def home
   end
