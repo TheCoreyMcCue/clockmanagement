c1a4346a00aaf64b8190ae05beb5c4a41c50190c
monica andreoli <monicandreoli@gmail.com>
2021-03-29 18:29:16 +0200

    form

--- app/javascript/plugin/htmlToText.js
+++ app/javascript/plugin/htmlToText.js
@@ -1,11 +1,11 @@
 const htmlToText = () => {
-  // const container = document.querySelector('.ranking-container');
+  const container = document.querySelector('.ranking-container');
 
-  // if (container) {
-  //   const data = container.dataset.ranking;
-  //   console.log(data);
-  //   container.insertAdjacentHTML("beforeend", `<div>${data}<div>`);
-  // }
+  if (container) {
+    const data = container.dataset.ranking;
+    console.log(data);
+    container.insertAdjacentHTML("beforeend", `<div>${data}<div>`);
+  }
 }
 
 export default htmlToText;
